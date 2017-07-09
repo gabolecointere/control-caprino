eeb7e7cb6cad420ceb86f12e9394d45f1564f8f2
Gabriel lecointere <gabolecointere21@gmail.com>
2017-07-09 18:15:01 -0400

    Setup of AdminLTE with Vue.js for SPA app

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="content-wrapper">
+        <section class="content-header">
+          <h1>
+            Titulo de la pagina
+            <small>Descripcion de la Pagina</small>
+          </h1>
+        </section>
+        <section class="content container-fluid">
+        <div id="app">
+          <img src="./assets/logo.png">
+          <h1></h1>
+          <h2>Essential Links</h2>
+          <ul>
+            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+            <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+          </ul>
+          <h2>Ecosystem</h2>
+          <ul>
+            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+          </ul>
+        </div>
+    </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
